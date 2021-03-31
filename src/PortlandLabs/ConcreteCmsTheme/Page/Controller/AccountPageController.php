4b92f52aa72a480ecbcdae51cdf38744ba5c98de
Andrew Embler <andrew@concrete5.org>
2021-03-31 16:12:34 -0700

    adding container back around default view.php in the concrete theme. adding a new view_full.php wrapper template for fancy pages like in community that need it unwrapped

--- src/PortlandLabs/ConcreteCmsTheme/Page/Controller/AccountPageController.php
+++ src/PortlandLabs/ConcreteCmsTheme/Page/Controller/AccountPageController.php
@@ -24,8 +24,6 @@ class AccountPageController extends CoreAccountPageController
     {
         parent::on_start();
 
-        // Override the theme
-        $this->setTheme('concrete_cms');
         $this->set('exclude_breadcrumb', true);
 
         $this->responseFactory = $this->app->make(ResponseFactory::class);
