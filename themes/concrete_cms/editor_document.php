2266d19fd82472c6ebcfd5b6abccd0b8724cbcb4
Fabian Bitter <fabian@bitter.de>
2021-04-01 21:03:20 +0200

    Add page templates for documentation site

---
+++ themes/concrete_cms/editor_document.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @project:   ConcreteCMS Theme
+ *
+ * @copyright  (C) 2021 Portland Labs (https://www.portlandlabs.com)
+ * @author     Fabian Bitter (fabian@bitter.de)
+ */
+
+defined('C5_EXECUTE') or die("Access Denied.");
+
+use Concrete\Core\View\View;
+
+/** @var View $this */
+
+$this->inc('document.php');
