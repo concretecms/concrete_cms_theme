8f75f1ee9aadba59ba30563cd03c505f25d7a13c
Andrew Embler <andrew@concrete5.org>
2021-04-26 14:29:27 -0700

    adding containers to xml

--- controller.php
+++ controller.php
@@ -26,7 +26,7 @@ class Controller extends Package
 {
     protected $pkgHandle = 'concrete_cms_theme';
     protected $appVersionRequired = '9.0';
-    protected $pkgVersion = '0.1.8';
+    protected $pkgVersion = '0.2.0';
     protected $pkgAllowsFullContentSwap = true;
     protected $pkgAutoloaderRegistries = [
         'src/PortlandLabs/ConcreteCmsTheme' => 'PortlandLabs\ConcreteCmsTheme',
