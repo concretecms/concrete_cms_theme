6f2b430443e8fe6db1c06942bccff4eb185b2906
Andrew Embler <andrew@concrete5.org>
2021-05-20 15:03:44 -0700

    adding solutions containers

---
+++ themes/concrete_cms/elements/containers/solution_section_white.php
@@ -0,0 +1,25 @@
+<?php
+
+defined('C5_EXECUTE') or die("Access Denied.");
+use Concrete\Core\Area\ContainerArea;
+
+?>
+
+<section class="section-solution section-white">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <?php
+                $area = new ContainerArea($container, 'Column One');
+                $area->display($c);
+                ?>
+            </div>
+            <div class="col-md-6">
+                <?php
+                $area = new ContainerArea($container, 'Column Two');
+                $area->display($c);
+                ?>
+            </div>
+        </div>
+    </div>
+</section>
