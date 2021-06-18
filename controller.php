a6f2782e0037719cd20a39becf0f718e14463a32
Andrew Embler <andrew@concrete5.org>
2021-06-18 10:44:21 -0700

    new-507

--- controller.php
+++ controller.php
@@ -28,7 +28,7 @@ class Controller extends Package
 {
     protected $pkgHandle = 'concrete_cms_theme';
     protected $appVersionRequired = '9.0';
-    protected $pkgVersion = '0.3.2';
+    protected $pkgVersion = '0.3.5';
     protected $pkgAllowsFullContentSwap = true;
     protected $pkgAutoloaderRegistries = [
         'src/PortlandLabs/ConcreteCmsTheme' => 'PortlandLabs\ConcreteCmsTheme',
---
+++ themes/concrete_cms/elements/containers/two_column_top_align_white.php
@@ -0,0 +1,11 @@
+<?php
+
+defined('C5_EXECUTE') or die("Access Denied.");
+
+View::element('two_column_container', [
+    'c' => $c,
+    'container' => $container,
+    'color' => 'white',
+    'leftColumnClass' => 'col-md-6',
+    'rightColumnClass' => 'col-md-6',
+], 'concrete_cms_theme');
