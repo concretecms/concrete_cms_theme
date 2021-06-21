191a740a22bfc8e9e92fb94550acb4c4aad0a406
Andrew Embler <andrew@concrete5.org>
2021-06-21 10:13:12 -0700

    Fix new-475

--- src/PortlandLabs/ConcreteCmsTheme/Navigation/FooterNavigationFactory.php
+++ src/PortlandLabs/ConcreteCmsTheme/Navigation/FooterNavigationFactory.php
@@ -24,43 +24,41 @@ class FooterNavigationFactory implements ApplicationAwareInterface
         $navigation = new Navigation();
         $navigation->add(new Item('{{marketing}}/about', t('About'), false, false, [
             new Item('{{marketing}}/about/features', t('Features')),
-            new Item('{{marketing}}/about/blog', t('Blog')),
             new Item('{{marketing}}/about/case-studies', t('Case Studies')),
             new Item('{{marketing}}/about/solutions', t('Solutions')),
-            new Item('{{marketing_org}}/history', t('History')),
-            new Item('{{marketing_org}}/vision', t('Vision')),
-            new Item('{{marketing_org}}/roadmap', t('Roadmap')),
-            new Item('{{marketing_org}}/governance', t('Governance')),
-            new Item('{{marketing_org}}/security', t('Security')),
+            new Item('{{marketing}}/about/blog', t('Blog')),
+            new Item('{{marketing}}/about/contact-us', t('Contact')),
             new Item('{{marketing}}/about/partners', t('Partners')),
             new Item('{{marketing}}/about/legal/terms-use', t('Terms of Use')),
-            new Item('{{marketing}}/about/legal/privacy-policy', t('Privacy Policy'))
+            new Item('{{marketing}}/about/legal/privacy-policy', t('Privacy Policy')),
+            new Item('{{marketing_org}}/security', t('Security')),
         ]));
         $navigation->add(new Item('{{marketing}}/get-started', t('Get Started'), false, false, [
-            new Item('{{marketing}}/get-started/try', t('Try it Now!')),
+            new Item('{{marketing}}/about/solutions', t('Solutions')),
+            new Item('{{marketing}}/hosting', t('Hosting')),
             new Item('{{marketing_org}}/download', t('Download')),
-            new Item('{{marketing}}/installation', t('Installation')),
+            new Item('{{documentation}}/developers/introduction/installation', t('Installation')),
         ]));
-        $navigation->add(new Item('{{marketplace}}/support', t('Marketplace'), false, false, [
-            new Item('{{marketplace}}/themes', t('Themes')),
-            new Item('{{marketplace}}/addons', t('Add-Ons')),
-            new Item('{{marketplace}}/starting-points', t('Starting Points')),
-            new Item('{{documentation}}/marketplace-installation', t('Installation Help')),
+        $navigation->add(new Item('{{marketing}}/extensions', t('Extensions'), false, false, [
+            new Item('{{marketplace}}/marketplace/addons', t('Add-ons')),
+            new Item('{{marketplace}}/marketplace/themes', t('Themes')),
+            new Item('{{marketing}}/extensions/related-projects', t('Related Projects')),
+            new Item('{{marketing}}/extensions/translate', t('Translations')),
             new Item('{{marketing}}/about/legal/refund-policy', t('Refund Policy')),
         ]));
         $navigation->add(new Item('{{marketing}}/support', t('Support'), false, false, [
+            new Item('{{marketing}}/support/hiring-help', t('Hiring Help')),
             new Item('{{documentation}}', t('Documentation')),
-            new Item('{{training}}', t('Training & Certification')),
-            new Item('{{gigs}}', t('Hire Help')),
-        ]));
-        $navigation->add(new Item('{{community}}', t('Community'), false, false, [
             new Item('{{forums}}', t('Forums')),
-            new Item('{{community}}/members', t('Members')),
-            new Item('{{translate}}', t('Translate Concrete')),
-            new Item('{{marketing_org}}/town-halls', t('Town Halls')),
-            new Item('{{marketing_org}}/get-involved', t('Get Involved')),
+            new Item('{{marketing}}/support/training-certification', t('Training & Certification')),
         ]));
-
+        $navigation->add(new Item('{{marketing}}/community', t('Community'), false, $activeSection === 'community', [
+            new Item('{{forums}}', t('Forums')),
+            new Item('{{community}}/members/directory', t('Search Members')),
+            new Item('{{marketing}}/extensions/translate', t('Translate')),
+            new Item('{{marketing_org}}/town-halls', t('Town Halls')),
+        ]));
+        
         $modifier = new NavigationModifier();
         $modifier->addModifier($this->app->make(SiteUrlPlaceholderModifier::class));
         return $modifier->process($navigation);
