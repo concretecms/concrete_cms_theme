a2b40e4bd040c65acef4e919e98a2382ff8e19ae
Michele Locati <michele@locati.it>
2022-03-09 15:10:33 +0100

    Fix domain of the ccm_cdd cookie
    The previous approach assumed that the domain was in form
    sub.domain.tld
    
    But this may not be the case (in particular on dev enviros).
    
    With this change, the domain part of the cookie will be:
    
    - "domain=domain"
      if the website domain is "domain"
    - "domain=domain.tld"
      if the website domain is "domain.tld"
    - "domain=.domain.tld"
      if the website domain is "sub.domain.tld"
    - "domain=.domain.tld"
      if the website domain is "sub1.sub2.domain.tld"

--- build/assets/themes/concrete_cms/js/main.js
+++ build/assets/themes/concrete_cms/js/main.js
@@ -264,8 +264,8 @@ window.addEventListener('load', () => {
             setTimeout(() => disclosure.remove(), 500)
 
             // Set a 100 year cookie
-            const sld = window.location.hostname.split('.').slice(-2).join('.')
-            setCookie('ccm_cdd=1;path=/;samesite=lax;domain=.' + sld + ';max-age=' + (31536000 * 100))
+            const sld = window.location.hostname.replace(/^.*(\.[^\.]+\.[^\.]+)$/, '$1')
+            setCookie('ccm_cdd=1;path=/;samesite=lax;domain=' + sld + ';max-age=' + (31536000 * 100))
         })
     }
 })
