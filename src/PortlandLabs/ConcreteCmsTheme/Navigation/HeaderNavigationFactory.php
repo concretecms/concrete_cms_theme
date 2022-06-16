8fd6927a1ba0406d398a0a103ab5c6905d02b077
Myq Larson <myq@portlandlabs.com>
2022-06-16 15:44:53 -0700

    update

--- src/PortlandLabs/ConcreteCmsTheme/Navigation/HeaderNavigationFactory.php
+++ src/PortlandLabs/ConcreteCmsTheme/Navigation/HeaderNavigationFactory.php
@@ -24,68 +24,67 @@ class HeaderNavigationFactory implements ApplicationAwareInterface, NavigationFa
     {
         $activeSection = $this->activeSection;
         // Determine if this navigation menu is for the concretecms.org site or not
-        $site_target = $_ENV['URL_SITE_MARKETING_OPENSOURCE'];
-        $site_detected = \Core::make('site')->getSite()->getSiteCanonicalURL();
+        $siteTarget = $_ENV['URL_SITE_MARKETING_OPENSOURCE'];
+        $siteDetected = \Core::make('site')->getSite()->getSiteCanonicalURL();
         // remove trailing slash if exists
-        if (strcmp('/', substr($site_detected, -1)) === 0 ) {
-            $site_detected = substr($site_detected, 0, -1);
+        if (strcmp('/', substr($siteDetected, -1)) === 0) {
+            $siteDetected = substr($siteDetected, 0, -1);
         }
-        $site_is_marketing_opensource = false;
-        if (strcmp($site_target, $site_detected) === 0) {
-            $site_is_marketing_opensource = true;
+        $siteIsMarketingOpensource = false;
+        if (strcmp($siteTarget, $siteDetected) === 0) {
+            $siteIsMarketingOpensource = true;
         }
 
         $navigation = new Navigation();
         // About
-        $navigation_about_menu = [];
-        if ($site_is_marketing_opensource) {
+        if ($siteIsMarketingOpensource) {
             // About menu for concretecms.org
-            $navigation_about_menu = [
-                new Item('{{marketing_org}}/vision',                   t('Vision')),
-                new Item('{{marketing_org}}/security',                 t('Security')),
+            $navigationAboutMenu = [
+                new Item('{{marketing_org}}/vision', t('Vision')),
+                new Item('{{marketing_org}}/security', t('Security')),
                 new Item('{{marketing_org}}/about/blog/core-releases', t('Releases')),
-                new Item('{{marketing_org}}/town-halls',               t('Town Halls')),
-                new Item('{{marketing_org}}/open-source-license ',     t('License')),
-                new Item('{{marketing_org}}/get-involved',             t('Get Involved')),
-                new Item('{{marketing_org}}/about/project-news',       t('Project News')),
+                new Item('{{marketing_org}}/town-halls', t('Town Halls')),
+                new Item('{{marketing_org}}/open-source-license ', t('License')),
+                new Item('{{marketing_org}}/get-involved', t('Get Involved')),
+                new Item('{{marketing_org}}/about/project-news', t('Project News')),
             ];
         } else {
             // About menu for all other sites
-            $navigation_about_menu = [
-                new Item('{{marketing}}/about/features',     t('Features')),
-                new Item('{{marketing}}/about/reviews',      t('Reviews')),
+            $navigationAboutMenu = [
+                new Item('{{marketing}}/about/features', t('Features')),
+                new Item('{{marketing}}/about/reviews', t('Reviews')),
                 new Item('{{marketing}}/about/case-studies', t('Case Studies')),
-                new Item('{{marketing}}/industries',         t('Industries')),
+                new Item('{{marketing}}/industries', t('Industries')),
                 new Item('{{marketing}}/about/applications', t('Applications')),
-                new Item('{{marketing}}/about/blog',         t('Blog')),
-                new Item('{{marketing}}/about/contact-us',   t('Contact')),
+                new Item('{{marketing}}/about/blog', t('Blog')),
+                new Item('{{marketing}}/about/contact-us', t('Contact')),
             ];
         }
-        $navigation->add(new Item('{{marketing}}/about', t('About'), false, false, $navigation_about_menu));
+        $navigation->add(new Item('{{marketing}}/about', t('About'), false, false, $navigationAboutMenu));
         // Get Started
         $navigation->add(new Item('{{marketing}}/get-started', t('Get Started'), false, false, [
-            new Item('{{marketing}}/hosting',                                             t('Hosting')),
-            new Item('{{marketing_org}}/download',                                        t('Download')),
+            new Item('{{marketing}}/hosting', t('Hosting')),
+            new Item('{{marketing_org}}/download', t('Download')),
             new Item('{{documentation}}/developers/introduction/installing-concrete-cms', t('Installation')),
         ]));
         // Extensions
         $navigation->add(new Item('{{marketing}}/extensions', t('Extensions'), false, false, [
-            new Item('{{marketplace}}/marketplace/addons',        t('Add-ons')),
-            new Item('{{marketplace}}/marketplace/themes',        t('Themes')),
+            new Item('{{marketplace}}/marketplace/addons', t('Add-ons')),
+            new Item('{{marketplace}}/marketplace/themes', t('Themes')),
             new Item('{{marketing}}/extensions/related-projects', t('Related Projects')),
-            new Item('{{marketing}}/extensions/translate',        t('Translations')),
+            new Item('{{marketing}}/extensions/translate', t('Translations')),
         ]));
         // Support
         $navigation->add(new Item('{{marketing}}/support', t('Support'), false, $activeSection === 'support', [
-            new Item('{{marketing}}/support/hiring-help',            t('Hiring Help')),
-            new Item('{{documentation}}',                            t('Documentation')),
-            new Item('{{forums}}',                                   t('Forums')),
+            new Item('{{marketing}}/support/hiring-help', t('Hiring Help')),
+            new Item('{{documentation}}', t('Documentation')),
+            new Item('{{forums}}', t('Forums')),
             new Item('{{marketing}}/support/training-certification', t('Training & Certification')),
         ]));
         // Community
         $navigation->add(new Item('{{marketing}}/community', t('Community'), false, $activeSection === 'community', [
-            new Item('{{forums}}',                         t('Forums')),
-            new Item('{{community}}/members/directory',    t('Search Members')),
+            new Item('{{forums}}', t('Forums')),
+            new Item('{{community}}/members/directory', t('Search Members')),
             new Item('{{marketing}}/extensions/translate', t('Translate')),
         ]));
 
