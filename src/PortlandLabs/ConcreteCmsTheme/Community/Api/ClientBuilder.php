4c287af1a63119f09c6e98e738c101b6f6d2a598
Andrew Embler <andrew@concrete5.org>
2024-01-24 09:19:26 -0800

    forgot a file

---
+++ src/PortlandLabs/ConcreteCmsTheme/Community/Api/ClientBuilder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace PortlandLabs\ConcreteCmsTheme\Community\Api;
+
+use Concrete\Core\Application\ApplicationAwareInterface;
+use Concrete\Core\Application\ApplicationAwareTrait;
+use GuzzleHttp\Client as GuzzleClient;
+use League\OAuth2\Client\Provider\GenericProvider;
+
+class ClientBuilder implements ApplicationAwareInterface
+{
+    use ApplicationAwareTrait;
+
+    public function isEnabled(): bool
+    {
+        return filter_var($_ENV['COMMUNITY_API_ENABLED'] ?? true, FILTER_VALIDATE_BOOL);
+    }
+
+    public function getClient(): GuzzleClient
+    {
+        $provider = new GenericProvider(
+            [
+                'clientId' => $_ENV['COMMUNITY_API_CLIENT_ID'],
+                'clientSecret' => $_ENV['COMMUNITY_API_CLIENT_SECRET'],
+                'redirectUri' => '',
+                'urlAccessToken' => $_ENV['URL_SITE_COMMUNITY'] . '/oauth/2.0/token',
+                'urlAuthorize' => $_ENV['URL_SITE_COMMUNITY'] . '/oauth/2.0/authorize',
+                'urlResourceOwnerDetails' => $_ENV['URL_SITE_COMMUNITY'] . '/ccm/api/1.0/account/info',
+            ]
+        );
+        $config = [
+            'grant_type' => 'client_credentials',
+            'scope' => [
+                'users:read',
+            ]
+        ];
+        $cache = $this->app->make('cache/expensive')->getPool();
+
+        $client = \Softonic\OAuth2\Guzzle\Middleware\ClientBuilder::build(
+            $provider,
+            $config,
+            $cache,
+            ['base_uri' => $_ENV['URL_SITE_COMMUNITY']]
+        );
+
+        return $client;
+    }
+}
